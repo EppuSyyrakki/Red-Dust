d517fe8f7e6fbc958708afc9da81ced3e106f485
Eppu Syyrakki <eppu.syyrakki@tuni.fi>
2022-03-19 20:29:43 +0200

    URP configuration

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -1,14 +1,11 @@
-﻿using System.Collections;
-using UnityEngine;
-
-namespace RedDust
+﻿namespace RedDust
 {
 	public static class Config
 	{
 		public class Navigation
 		{
 			public const float MoveTargetTreshold = 0.5f;
-			public const float MaxNavMeshProjection = 1f;
+			public const float MaxNavMeshProjection = 1.5f;
 			public const float MovingThreshold = 0.05f;
 			public const float WalkMulti = 0.6f;
 			public const float CrouchMulti = 0.5f;
@@ -20,6 +17,32 @@ namespace RedDust
 			public const string Velocity = "velocity";
 			public const string Turning = "turning";
 			public const string Crouched = "crouched";
+		}
+
+		public class Input
+		{
+			public const string MapTactical = "Tactical";
+			public const string MapMenu = "Menu";
+			public const string ActionMoveCursor = "MoveCursor";
+			public const string ActionInteract = "Interact";
+			public const string ActionDrag = "Drag";
+			public const float MouseCastRange = 200f;
+		}
+
+		public class AI
+		{
+			public const float FollowUpdateInterval = 1f;
+		}
+
+		public class Layers
+		{
+			public const int Ground = 1 << 3;
+			public const int Character = 1 << 6;
+		}
+
+		public class Tags
+		{
+			public const string TacticalControl = "TacticalControl";
 		}
 	}
 }
